9eb3e05ca566914c8c92d2a6cad1f270cfdbdacc
Simran thakur <shivangithakur7300@gmail.com>
2024-12-16 23:32:41 +0530

    updated setup file

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path ) as f:
         requirements = f.readlines()
-        requirements = [req.replace("/n","")for req in requirements]
+        requirements = [req.replace("\n","")for req in requirements]
 
 
         if HYPHEN_E_DOT in requirements:
@@ -18,10 +18,10 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 
-setup(name='Visibility_Prediction' ,
+setup(name='Climate_Visibility_Prediction' ,
       version='1.0',
-      author='RA2332014010177_RA2332014010178',
-      author_email='shrishkamboz@gmail.com',
+      author='Simran Thakur',
+      author_email='shivangithakur7300@gmail.com',
       packages= find_packages(),
       install_requires=get_requirements('requirements.txt')
       )
